4ca61b18ca5cba4d510358026d30d9f9fd3eb31d
alan lima <alan.silva1f@gmail.com>
2021-12-19 15:22:50 -0300

    commitando o menu lateral

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
 const menuHamburguer = document.querySelector('.hamburguer');
-const navbar = document.querySelector('.navbar ul');
+const navbar = document.querySelector('.navbar');
 
 menuHamburguer.onclick = () =>{
     menuHamburguer.classList.toggle('active');
     navbar.classList.toggle('active');
-};
-
-window.onscroll.onclick = () =>{
-    menuHamburguer.classList.remove('active');
-    navbar.classList.remove('active');
 };
